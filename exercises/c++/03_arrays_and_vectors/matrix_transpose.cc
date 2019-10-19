c95fc75ac8a964c0af2be2aa5fd4bfdb62c7c6f2
Kevin Wolz <kevguitar93@gmail.com>
2019-10-19 19:05:13 +0200

    Solved exercises 3.1 and 3.2

--- exercises/c++/03_arrays_and_vectors/matrix_transpose.cc
+++ exercises/c++/03_arrays_and_vectors/matrix_transpose.cc
@@ -1,45 +1,74 @@
 #include<iostream>
-#include<iomanip> // std::setw(size_t width)
+#include<iomanip> // std::setw(std::size_t width)
 #include<array>
 #include<utility>   // std::swap
+#include<sstream>   // read content from string
 
 
-// pass argument of type T by reference
 template <typename T> 
-T transpose_matrix(const T& original, const size_t rows, const size_t cols) {
-  // initialize modified and set it equal to original  
-  T modified{original};
+T matrix_transpose(const T& original, std::size_t rows, std::size_t cols);
+
+template <typename T> 
+T swap_example(const T& original);
+
+template <typename T> 
+void print_matrix(const T& matrix, std::size_t rows, std::size_t cols);
 
 
-  // TODO:
-  // The arguments of std::swap are not accepted; need to replace them by the
-  // correct formulation.
-  for (size_t i=0; i<rows; ++i) {
-    for (size_t j=i; j<cols; ++j) {
-      // Swaps the values assigned to the addresses corresponding to the 
-      // (i,j) element and the (j,i).
-      std::swap(*(&original + i*cols + j), *(&original + j*cols + i));
-    }
+int main(int argc, char* argv[]) {
+  // First version: No command line arguments.
+  /*const std::size_t c{3};
+  const std::size_t r{3};*/
+
+  // Second version: command line arguments
+  std::size_t args[2];
+  for (auto i{0}; i<2; ++i) {
+    std::istringstream is{argv[i+1]};
+    is>>args[i];
   }
-  return modified;
-}
+  std::size_t r{args[0]}, c{args[1]};
+  std::cout<<"rows = "<<r<<", cols = "<<c<<std::endl;
 
-int main() {
-  const size_t N{9};
-  std::array<int, N> matrix{1,2,3,4,5,6,7,8,9}; 
+  // An array size must be known at compile time, so no variable here.
+  std::array<int, 9> matrix {1,2,3,4,5,6,7,8,9};
   //std::array<int, N> matrix_t;    // not needed, since auto can be used
-
-  for (const auto x : matrix) 
-    std::cout<<std::setw(3)<<x<<" ";
-  std::cout<<std::endl;
-    
-  auto matrix_t = transpose_matrix(matrix, 3, 3);
-
-  for (const auto x : matrix_t) 
-    std::cout<<std::setw(3)<<x<<" ";
-  std::cout<<std::endl;
-
+  std::cout<<"original matrix:"<<std::endl;
+  print_matrix(matrix, r, c);
+  auto matrix_t = matrix_transpose(matrix, r, c);
+  std::cout<<"transposed matrix:"<<std::endl;
+  print_matrix(matrix_t, r, c);
 
   return 0;
 }
 
+template <typename T> 
+T swap_example(const T& original) {
+  T modified{original};
+  std::swap(modified[0], modified[1]);
+
+  return modified;
+}
+
+template <typename T> 
+T matrix_transpose(const T& original, std::size_t rows, std::size_t cols) {
+  T modified{original};
+  std::size_t rx{0};
+  for (; rx<rows; ++rx) {
+    for (auto cx{rx}; cx<cols; ++cx) {
+      std::swap(modified[rx*rows + cx], modified[cx*rows + rx]);
+    }
+  }
+
+  return modified;
+}
+
+template <typename T> 
+void print_matrix(const T& mat, std::size_t rows, std::size_t cols) {
+  std::size_t rx{0};
+  for (; rx<rows; ++rx) {
+    for (std::size_t cx{0}; cx<cols; ++cx) {
+      std::cout<<std::setw(2)<<mat[rx*rows + cx]<<" ";
+    }
+    std::cout<<std::endl;
+  }
+}
