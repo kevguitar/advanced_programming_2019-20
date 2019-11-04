5f37e89223189da6ed01e808d88c88aec469c027
Kevin Wolz <kevguitar93@gmail.com>
2019-11-04 18:13:00 +0100

    Comtinued with linked_list.cc

--- exercises/c++/05_copy_move/linked_list.cc
+++ exercises/c++/05_copy_move/linked_list.cc
@@ -1,4 +1,4 @@
-#include <algorithm>  // std::copy
+#include <algorithm>
 #include <iostream>
 #include <memory>
 
@@ -19,6 +19,9 @@ class List {
 
   // delete all the nodes of the list (decl only)
   void reset();
+
+  // display function: forwards the pointers to all nodes to ostream
+  std::ostream& operator<<(std::ostream& os, const List<T>& l);
 
   // default ctor
   List() : node{}, head{}, size{} { std::cout << "custom ctor\n"; }
@@ -101,39 +104,40 @@ class List {
 };
 
 
-// TODO
 template <class value_type>
 void List::insert(const value_type& v, const Insertion_method m) {
   if (m==Insertion_method::push_front) {
-    // create node first;
-    // copy address of pointer head to tmp_pointer;
-    // first.next = tmp_pointer;    // Do I need to invoke std::copy here?
-    // first.value = v;             
-    // head = &v;  
+    auto first{new node};
+    auto tmp = head; 
+    first->next = tmp;  // could I use {} initializers here?    
+    first->value = v;   // ... and here?            
+    head = &first;  
   } else {
-    // create node last;
-    // loop through nodes (pointer by pointer) until node.pointer==nullptr
-    // last.value = v;          // Do I need to invoke std::copy here?
-    // set node.pointer = &last.value;
-    // last.next = nullptr;
+    auto last{new node};
+    auto tmp = head;
+    while ((*tmp)->next) { tmp = (*tmp)->next; } // while not null, redirect tmp
+    last->value = v;                // for last element, set value to v,
+    (last->next).reset(nullptr);    // point last node to nullptr,
+    (*tmp)->next = &last;           // point penultimate node to last node                
   }
 }
 
-// TODO
+
 template <class value_type>
 void List::insert(value_type&& v, const Insertion_method m) {
   if (m==Insertion_method::push_front) {
-    // node first{std::move()};
-    // copy address of pointer head to tmp_pointer;
-    // first.next = tmp_pointer;    // Do I need to invoke std::copy here?
-    // first.value = v;             
-    // head = &v;  
+    auto first{new node};
+    auto tmp = head; 
+    first->next{tmp};
+    first->value{std::move(v)};             
+    head = &first;  
   } else {
-    // create node last;
-    // loop through nodes (pointer by pointer) until node.pointer==nullptr
-    // last.value = v;          // Do I need to invoke std::copy here?
-    // set node.pointer = &last.value;
-    // last.next = nullptr;
+    auto last{new node};
+    auto tmp = head;
+    while ((*tmp)->next) { tmp = (*tmp)->next; } // while not null, redirect tmp
+    last->value{std::move(v)};                // for last element, set value to v,
+    (last->next).reset(nullptr);    // point last node to nullptr,
+    (*tmp)->next = &last;           // point penultimate node to last node                
   }
 }
 
@@ -141,27 +145,27 @@ void List::insert(value_type&& v, const Insertion_method m) {
 // deletes all the nodes of the list
 template <class value_type>
 void List::reset() {
-  // deletes node pointers
-  unique_ptr<node> foot{head};
-  if (head) { head = (*foot).next; }  // if head!=nullptr push head to next node
-  while(head) {                       // check if head==nullptr;
-    head = (*head).next;              // push head to next node
-    (*foot).next.reset(nullptr);      // delete old head
+  // TODO:
+  // Sufficient to delete only the pointers (without invoking memory leask)? 
+  auto foot = head;
+  if (head) { head = (*foot)->next; }  // if head!=nullptr push head to next node
+  while (head) {                       // check if head==nullptr;
+    head = (*head)->next;              // push head to next node
+    ((*foot)->next).reset(nullptr);    // delete old head
   }
-  // set size to 0;
   size = 0;
-
 }
 
-// TODO
-template <class T>
-std::ostream& operator<<(std::ostream& os, const List<T>& l) {
-}
-
-/*template <typename T>
-std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
-  for (const auto& x : v)
-    os << x << " ";
-  os << std::endl;
-  return os;
-}*/
+// A range-for is not needed, since linked list can be trivially looped 
+// through by a succesive sequence of pointers connecting each node, ending at
+// a null pointer. Instead, we need a display function 
+template <class value_type>
+std::ostream& List::operator<<(std::ostream& os, const List<T>& l) {
+    auto temp = head;
+    while (temp) { // while temp is not nullptr
+      os<<(*temp)->value<<" ";
+      temp = (*temp)->next;
+    }
+    os<<std::endl;
+    return os;
+} 
