9421335e559673d2285a9d932b47c12dfecd5aa0
Kevin Wolz <kevguitar93@gmail.com>
2019-11-08 21:30:41 +0100

    Attempt to cure matrix_transpose.cc (no success)

--- exercises/c++/03_arrays_and_vectors/matrix_transpose.cc
+++ exercises/c++/03_arrays_and_vectors/matrix_transpose.cc
@@ -6,51 +6,33 @@
 
 
 template <typename T> 
-T matrix_transpose(const T& original, std::size_t rows, std::size_t cols);
-
-template <typename T> 
-T swap_example(const T& original);
+T& matrix_transpose(const T& original, std::size_t rows, std::size_t cols);
 
 template <typename T> 
 void print_matrix(const T& matrix, std::size_t rows, std::size_t cols);
 
 
 int main(int argc, char* argv[]) {
-  // First version: No command line arguments.
-  /*const std::size_t c{3};
-  const std::size_t r{3};*/
-
-  // Second version: command line arguments
-  std::size_t args[2];
+  std::string[2] tmp;
   for (auto i{0}; i<2; ++i) {
     std::istringstream is{argv[i+1]};
-    is>>args[i];
+    is>>tmp[i];
   }
-  std::size_t r{args[0]}, c{args[1]};
+  std::size_t r{tmp[0]}, c{tmp[1]};
   std::cout<<"rows = "<<r<<", cols = "<<c<<std::endl;
 
-  // An array size must be known at compile time, so no variable here.
-  std::array<int, 9> matrix {1,2,3,4,5,6,7,8,9};
-  //std::array<int, N> matrix_t;    // not needed, since auto can be used
+/*  std::array<int, 9> matrix {1,2,3,4,5,6,7,8,9};
   std::cout<<"original matrix:"<<std::endl;
   print_matrix(matrix, r, c);
   auto matrix_t = matrix_transpose(matrix, r, c);
   std::cout<<"transposed matrix:"<<std::endl;
-  print_matrix(matrix_t, r, c);
+  print_matrix(matrix_t, r, c);*/
 
   return 0;
 }
 
 template <typename T> 
-T swap_example(const T& original) {
-  T modified{original};
-  std::swap(modified[0], modified[1]);
-
-  return modified;
-}
-
-template <typename T> 
-T matrix_transpose(const T& original, std::size_t rows, std::size_t cols) {
+T& matrix_transpose(const T& original, std::size_t rows, std::size_t cols) {
   T modified{original};
   std::size_t rx{0};
   for (; rx<rows; ++rx) {
