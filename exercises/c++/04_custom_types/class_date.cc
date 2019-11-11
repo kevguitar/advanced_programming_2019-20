4fe41776537ba912f2b102b4a92f4b2a1282298f
Kevin Wolz <kevguitar93@gmail.com>
2019-11-11 16:03:23 +0100

    Finished class "Date"

--- exercises/c++/04_custom_types/class_date.cc
+++ exercises/c++/04_custom_types/class_date.cc
@@ -6,93 +6,114 @@
 enum class months { jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, 
                     dec };
 
+// prefix increment months (copy overload)
+months& operator++(months& m) {
+  //std::cout<<"months - copy inc"<<std::endl;
+  if (m==months::dec) m = months::jan; 
+  else m = static_cast<months>(static_cast<int>(m) + 1);
+  return m;
+}
+
+// prefix increment months (move overload)
+months& operator++(months&& m) {
+  //std::cout<<"months - move inc"<<std::endl;
+  if (m==months::dec) m = months::jan; 
+  else m = static_cast<months>(static_cast<int>(m) + 1);
+  return m;
+}
+
+
+
 class Date {
  private:
   unsigned int _day, _year;
   months _month;
+
+  // (decl only)
   void increment();
 
  public:
-  Date(unsigned int d, unsigned int y, months m);
-  ~Date();
-  unsigned int day() { return _day; }
-  months month() { return _month; }
-  unsigned int year() { return _year; }
-  void add_days(const unsigned int n);
+  // default ctor
+  Date(unsigned int d, unsigned int y, months m) 
+      : _day{d}, _year{y}, _month{m} {};
+  ~Date() = default;
+  unsigned int day() const { return _day; }  /* "const" needed to mark 
+                                                "day()" a "safe" function. */    
+  months month() const { return _month; }
+  unsigned int year() const { return _year; }
+
+  void print() { 
+    std::cout<<_day<<"/"<<static_cast<int>(_month)+1<<"/"<<_year<<std::endl; 
+  }
+  void add_days(const unsigned int n) { for (int i{0}; i<n; ++i) increment(); }
 };
 
-Date::Date(unsigned int d, unsigned int y, months m) 
-    : _day{d}, _year{y}, _month{m} {
-  std::cout<<"Construct 'Date'..."<<std::endl;
+bool operator==(const Date& lhs, const Date& rhs) {
+  bool d{lhs.day()==rhs.day()}, m{lhs.month()==rhs.month()}, 
+       y{lhs.year()==rhs.year()};
+  return (d && m && y);
 }
 
-Date::~Date() {
-  std::cout<<"Destruct 'Date'..."<<std::endl;
+bool operator!=(const Date& lhs, const Date& rhs) {
+  return !(lhs==rhs);
 }
 
-void Date::increment() {
-  // TODO: Implement overloaded operator++ for months!
-  switch(_month){
-    case months::jan: case months::mar: case months::may: case months::jul: 
-    case months::aug: case months:oct:
-      if (_day==31) { 
-        _day = 1; 
-        _month++; // TODO!!
-      } else _day++;
-    case months:apr: months::jun: months::sep: months::nov:
-      if (_day==30) {
-        _day = 1;
-        _month++; // TODO!!
-      } else _day++;
-    case months::feb:
-      if (day==28) {
-        _day = 1;
-        _month++;
-      } else _day++;
-    case months::dec:
-      if (day==31) {
-        day = 1;
-        _month = months::jan;
-        }
-      }
-
-  }
-
+std::ostream& operator<<(std::ostream& os, const Date& d) {
+  os<<d.day()<<"/"<<static_cast<int>(d.month())+1<<"/"<<d.year()<<std::flush;
+  return os;
 }
-
-void Date::add_days(const unsigned int n) {
-  for (int i{0}; i<n; ++i) increment();
-}
-
-void 
 
 
 int main() {
-  Date xmas{24, 2019, months::dec};
-  //xmas.increment();
-  xmas.add_days(4);
-  int m{static_cast<int>(xmas.month())};
-  std::cout<<"xmas.month() = "<<m<<std::endl;
+  Date d1{24, 2019, months::dec};
+  Date d2{d1};
+  std::cout<<"d1 = "<<d1<<", d2 = "<<d2<<std::endl;
+  std::cout<<"(d1==d2) = "<<(d1==d2)<<std::endl;
+  std::cout<<"(d1!=d2) = "<<(d1!=d2)<<std::endl;
+  
+  d1.add_days(365);
+  std::cout<<"d1 = "<<d1<<", d2 = "<<d2<<std::endl;
+  std::cout<<"(d1==d2) = "<<(d1==d2)<<std::endl;
+  std::cout<<"(d1!=d2) = "<<(d1!=d2)<<std::endl;
 
   return 0;
 }
 
 
-/*
-int a;
-...
-switch (a){
-  case 0: case 2: case 4:
-  case 6: case 8: case 10:
-    std::cout << "a is even\n";
-    break;
 
-  case 1: case 3: case 5:
-  case 7: case 9:
-    std::cout << "a is odd\n";
-    break;
-  default:
-    std::cout << "a > 10\n";
-    break;
+void Date::increment() {
+  switch(_month){
+    case months::jan: case months::mar: case months::may: case months::jul: 
+    case months::aug: case months::oct:
+      if (_day==31) { 
+        _day = 1; 
+        ++_month;
+      } else ++_day;
+      break;
+    case months::apr: case months::jun: case months::sep: case months::nov:
+      if (_day==30) {
+        _day = 1;
+        ++_month;
+        
+      } else ++_day;
+      break;
+    case months::feb:
+      if (_day==28) {
+        _day = 1;
+        ++_month;
+        
+      } else ++_day;
+      break;
+    case months::dec:
+      if (_day==31) {
+        _day = 1;
+        ++_month;
+        ++_year;
+      } else ++_day;
+      break;
+    default:
+      std::cout<<"Error in date format."<<std::endl;
+      break;
+  }
 }
-*/
+
